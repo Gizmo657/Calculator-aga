c5de0bac42b7fb56c75060ca063414eec0649b87
Gizmo657 <90689095+Gizmo657@users.noreply.github.com>
2024-03-16 14:28:06 +0300

    SEC

--- calc2.py
+++ calc2.py
@@ -4,17 +4,14 @@ form1 = Tk()
 form1.title("Calc2")
 form1.geometry("260x350")
 
-def make1():
-    X=x
-
 tex = ttk.Label()
 entr = ttk.Entry()
 bpl = ttk.Button(text="+", command=make1)
-bmin = ttk.Button(text="-", command=make1)
-bmul = ttk.Button(text="x", command=make1)
-bdiv = ttk.Button(text=":", command=make1)
-beq = ttk.Button(text="=", command=make1)
-bcl = ttk.Button(text="CE", command=make1)
+bmin = ttk.Button(text="-", command=make2)
+bmul = ttk.Button(text="x", command=make3)
+bdiv = ttk.Button(text=":", command=make4)
+beq = ttk.Button(text="=", command=makeq)
+bcl = ttk.Button(text="CE", command=makec)
 
 tex.pack()
 entr.pack()
